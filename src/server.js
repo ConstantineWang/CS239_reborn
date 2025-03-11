3f9fab08a440375f595dc7a0c9a57bf6a04678b0
EC2 Default User <ec2-user@ip-172-31-34-69.ec2.internal>
2025-03-11 04:21:41 +0000

    nit

--- src/server.js
+++ src/server.js
@@ -110,7 +110,7 @@ async function processRequest(name) {
 
   try {
     // Initialize faast module with timeout parameter
-    m = await faast("local", funcs, {
+    m = await faast("aws", funcs, {
       memorySize: currentOptimalConfig.memorySize,
       maxConcurrency: currentOptimalConfig.concurrency,
       timeout: currentOptimalConfig.timeout, // Add timeout in seconds
